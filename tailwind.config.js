b1fcb8a4b73d3450205cd0eda6d09a3bb7ae7b5e
E$ <eric@adboomgrp.com>
2025-10-26 11:08:07 -0400

    revert font size change

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,21 +19,6 @@ export default {
           end: '#f5576c'
         }
       },
-      fontSize: {
-        'xs': ['0.8125rem', { lineHeight: '1.25rem' }],      // 13px (was 12px)
-        'sm': ['0.9375rem', { lineHeight: '1.375rem' }],     // 15px (was 14px)
-        'base': ['1.0625rem', { lineHeight: '1.625rem' }],   // 17px (was 16px)
-        'lg': ['1.1875rem', { lineHeight: '1.75rem' }],      // 19px (was 18px)
-        'xl': ['1.3125rem', { lineHeight: '1.875rem' }],     // 21px (was 20px)
-        '2xl': ['1.5625rem', { lineHeight: '2rem' }],        // 25px (was 24px)
-        '3xl': ['1.9375rem', { lineHeight: '2.375rem' }],    // 31px (was 30px)
-        '4xl': ['2.375rem', { lineHeight: '2.75rem' }],      // 38px (was 36px)
-        '5xl': ['3.0625rem', { lineHeight: '1' }],           // 49px (was 48px)
-        '6xl': ['3.8125rem', { lineHeight: '1' }],           // 61px (was 60px)
-        '7xl': ['4.8125rem', { lineHeight: '1' }],           // 77px (was 72px)
-        '8xl': ['6.0625rem', { lineHeight: '1' }],           // 97px (was 96px)
-        '9xl': ['8.0625rem', { lineHeight: '1' }],           // 129px (was 128px)
-      },
       screens: {
         'xs': '475px',
       },
